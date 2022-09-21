a86b7034f18c4ce17c78bc26cd3d3157d38d7b89
Mobborn02 <64135655+Mobborn02@users.noreply.github.com>
2022-09-21 20:27:13 +0200

    Update HORUS.py

--- David/HORUS.py
+++ David/HORUS.py
@@ -288,3 +288,27 @@ def duploremover(a, b):
     for element in duplicates:
         pile.remove(str(element))
     return pile
+
+
+def proteinweight(protein):
+    '''
+    This function calculates the weight of a protein
+
+    Parameters
+    ----------
+    protein : A string of amino acids
+
+    Returns
+    -------
+    weight : The weight of the input protein in kDa
+
+    '''
+    weight = 0
+    table = {
+        'A':89, 'R':174, 'N':132, 'D':133, 'B':133, 'C':121, 'Q':146, 'E':147, 
+        'Z':147, 'G': 75, 'H':155, 'I':131, 'L':131, 'K':146, 'M':149, 'F':165, 
+        'P':115, 'S':105, 'T':119, 'W':204, 'Y':181, 'V':117
+        }
+    for i in protein:
+        weight += table[i]
+    return weight
