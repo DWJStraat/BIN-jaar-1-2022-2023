9cba140d25fc1c77e9c6a8311f0f1c1ab4716a01
Mobborn02 <dstraat1@gmail.com>
2022-10-05 11:31:19 +0200

    Update HORUS.py

--- David/HORUS.py
+++ David/HORUS.py
@@ -6,7 +6,7 @@ Created on Tue Sep  6 15:16:28 2022
 
 Function: A collection of functions, written by David Straat
 
-TODO: 
+TODO:
     - find a way to better group functions
     - remove Dutch comments
     - update cutter
@@ -18,15 +18,16 @@ from colorama import Style
 from datetime import datetime
 import RPi.GPIO as GPIO
 
+
 def read_fna(file):
     '''
-    This function takes a file, removes the first line, and outputs the 
+    This function takes a file, removes the first line, and outputs the
     contents as a single string.
-    
+
     Parameters
     ----------
     file : The FASTA file to be examined in string format
-    
+
     Returns
     -------
     string : The string found within the FASTA file.
@@ -38,7 +39,8 @@ def read_fna(file):
         # Removes extra spaces and newlines
         string = ''.join(line).strip()
         string = ''.join(string.splitlines())
-        return string
+    return string
+
 
 def gpio_reader(Pin, loops):
     '''
@@ -46,7 +48,7 @@ def gpio_reader(Pin, loops):
 
     Parameters
     ----------
-    Pin : The 
+    Pin : The
     loops : TYPE
         DESCRIPTION.
 
@@ -56,26 +58,26 @@ def gpio_reader(Pin, loops):
         DESCRIPTION.
 
     '''
-    reader = [[],[]]
+    reader = [[], []]
     RECEIVE_PIN = Pin
-    
-    
+
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(RECEIVE_PIN, GPIO.IN)
     i = 0
     while i < loops:
-        time= datetime.now()
+        time = datetime.now()
         timestring = time.strftime('%f')
         reader[0].append(timestring)
         a = GPIO.input(RECEIVE_PIN)
         b = str(a)
         reader[1].append(b)
-        i+=1
+        i += 1
     return reader
+
 
 def agct_count(string):
     '''
-    This function counts the A, G, C, and T in the gene provided. 
+    This function counts the A, G, C, and T in the gene provided.
 
     Parameters
     ----------
@@ -87,7 +89,7 @@ def agct_count(string):
     G : The amount of G in the string
     C : The amount of C in the string
     T : The amount of T in the string
-    length : The length of the gene 
+    length : The length of the gene
 
     '''
     # Counts A's, G's, C's and T's
@@ -99,6 +101,7 @@ def agct_count(string):
     length = A+G+C+T
     # Outputs the values
     return A, G, C, T, length
+
 
 def type_identify(string):
     '''
@@ -120,18 +123,19 @@ def type_identify(string):
         if string[i] == 'M':
             type = "Protein"
             scan = True
-        elif string[i] in ["A","G","C","T"]:    
+        elif string[i] in ["A", "G", "C", "T"]:
             type = "DNA"
             scan = True
         else:
             i = i + 1
 
     if scan == False:
-        print ("Not recognized as a gene or protein.")
+        print("Not recognized as a gene or protein.")
     print(type)
     return type
 
-def gc_percent(C,G,length):
+
+def gc_percent(C, G, length):
     '''
     This function calculates the GC% of the gene
 
@@ -156,17 +160,18 @@ def gc_percent(C,G,length):
 
 def pcr_gc_calc(GC, length):
     '''
-    
+
 
     Parameters
     ----------
     GC : The GC% of the gene, in 0.xxx
-    
+
     length : The length of the gene
 
     Returns
     -------
-    meltingtemp : The temperature at which the DNA denaturates into two strings in degrees Celsius
+    meltingtemp : The temperature at which the DNA denaturates into two strings
+    in degrees Celsius
 
     '''
     totalGC = GC*length
@@ -174,18 +179,19 @@ def pcr_gc_calc(GC, length):
     meltingtemp = 4*totalGC + 2*totalAT
     return meltingtemp
 
+
 def snip(string, site, snipspot):
     '''
-    Determines the places where a specific restrictor enzyme cut the DNA, and 
+    Determines the places where a specific restrictor enzyme cut the DNA, and
     outputs the new, snipped DNA
 
     Parameters
     ----------
     string : The DNA to be examined.
     site : The site where the restrictor enzyme cuts the DNA.
-    snipspot : How many bases from the start of the cutting site the restrictor 
+    snipspot : How many bases from the start of the cutting site the restrictor
     enzyme cuts.
-    
+
     Returns
     -------
     output : The "cut" DNA string, with a ^ on the spot where it cuts
@@ -201,10 +207,11 @@ def snip(string, site, snipspot):
     snips = []
     output = ''
     nextstart = 0
-    # Als er tenminste 1 site aanwezig is, scant de While loop de sequentie, 
-    # returned de locaties van de sites, en stopt een ^ op de plaats van de cut.
+    # Als er tenminste 1 site aanwezig is, scant de While loop de sequentie,
+    # returned de locaties van de sites, en stopt een ^ op de plaats van de
+    # cut.
     if i > 0:
-        while i > 0 :
+        while i > 0:
             snip = string.find(site, nextstart)
             output = string[start:snip-1+snipspot] + '^'
             snips.append(snip)
@@ -212,42 +219,42 @@ def snip(string, site, snipspot):
             nextstart = snip + snipspot
         output = output + string[snip-1+snipspot:]
     else:
-    # Als er geen site aanwezig is, output de functie dezelfde string
+        # Als er geen site aanwezig is, output de functie dezelfde string
         output = string
-    
 
-    return output, snips 
+    return output, snips
+
 
 def cutter(DNA, enzymfile):
     '''
-    A function that can be used to process restriction enzymes found in a txt 
+    A function that can be used to process restriction enzymes found in a txt
     file on a string of DNA
 
     Parameters
     ----------
     DNA : The string of DNA to be analyzed
-    enzymfile : The txt file containing the names of the restriction enzymes 
+    enzymfile : The txt file containing the names of the restriction enzymes
                 and their restriction site.
     Examples of formatting: DdeI C^TGAG
-    
+
     Returns
     -------
-    cutDNA : The DNA after having been cut by the restriction enzymes entered, 
+    cutDNA : The DNA after having been cut by the restriction enzymes entered,
              in list format
     cuttingenzymes : The enzymes that cut the DNA, in list format
 
     '''
-    
-    bestand = open (enzymfile)
+
+    bestand = open(enzymfile)
     string = DNA
     cuttingenzymes = []
     read = True
     while read == True:
         line = bestand.readline().strip()
-        if line != '':    
+        if line != '':
             enzym, seq = line.split()
             site = seq
-            site = site.replace("^","")
+            site = site.replace("^", "")
             sites = DNA.count(site)
             if sites > 0:
                 string = string.replace(site, seq)
@@ -257,9 +264,11 @@ def cutter(DNA, enzymfile):
     cutDNA = string.split("^")
     return cutDNA, cuttingenzymes
 
+
 def duploremover(a, b):
     '''
-    A function that removes elements found in both lists and return a list containing all unique values
+    A function that removes elements found in both lists and return a list
+    containing all unique values
 
     Parameters
     ----------
@@ -274,10 +283,11 @@ def duploremover(a, b):
     pile = list(dict.fromkeys(pile))
     aset = set(a)
     bset = set(b)
-    duplicates = list(set(max(aset,bset)).intersection(min(aset,bset)))
+    duplicates = list(set(max(aset, bset)).intersection(min(aset, bset)))
     for element in duplicates:
         pile.remove(str(element))
     return pile
+
 
 def protein_weight(protein):
     '''
@@ -299,12 +309,12 @@ def protein_weight(protein):
     # Creates a dictionary with the molecular weights of each amino acid
     table = {
         'A': 89.09, 'C': 121.2, 'D': 133.1, 'E': 147.1, 'F': 165.2,
-        'G': 75.07, 'H': 155.2, 'I': 131.2, 'K': 146.2, 'L': 131.2, 
-        'M': 149.2, 'N': 132.1, 'P': 115.1, 'Q': 146.1, 'R': 174.2, 
-        'S':105.09, 'T': 119.1, 'V': 117.1, 'W': 204.2, 'Y': 181.2
-        }
-    # For each amino acid, compares the abbreviation of the amino acid with 
-    # the weight found in the table dictionary, and adds the weight to the 
+        'G': 75.07, 'H': 155.2, 'I': 131.2, 'K': 146.2, 'L': 131.2,
+        'M': 149.2, 'N': 132.1, 'P': 115.1, 'Q': 146.1, 'R': 174.2,
+        'S': 105.09, 'T': 119.1, 'V': 117.1, 'W': 204.2, 'Y': 181.2
+    }
+    # For each amino acid, compares the abbreviation of the amino acid with
+    # the weight found in the table dictionary, and adds the weight to the
     # weight variable
     for i in protein:
         weight += table[i]
@@ -314,9 +324,11 @@ def protein_weight(protein):
     weight = weight - (18.0153*(length-1))
     return weight
 
+
 def qr_generator(string, name):
     '''
-    This function generates a QR code based on the input in the working directory
+    This function generates a QR code based on the input in the working
+    directory
     Uses qrcode
 
     Parameters
@@ -332,7 +344,8 @@ def qr_generator(string, name):
     img = qrcode.make(string)
     type(img)
     img.save(f'{name}.png')
-    
+
+
 def qr_read(name):
     '''
     This function reads a QR code and outputs as a string
@@ -347,13 +360,14 @@ def qr_read(name):
     string : Output in string format
 
     '''
-    
+
     image = cv2.imread(name)
     detect = cv2.QRCodeDetector()
     string, points, qrcode = detect.detectAndDecode(image)
     return string
 
-def compare (string1, string2):
+
+def compare(string1, string2):
     '''
     Compares two strings, colors the overlapping characters green, the others
     red, and calculates the overlap in %
@@ -377,7 +391,7 @@ def compare (string1, string2):
     i = 0
     compare = 0
     # Calculates the length of the longest string
-    length = max(len(string1),len(string2))
+    length = max(len(string1), len(string2))
     # Loops through each character in the strings
     while i < length:
         # If one string is shorter than the other, it'll compensate with -'s
@@ -385,37 +399,38 @@ def compare (string1, string2):
         try:
             v1 = string1[i]
         except:
-            v1 = "-"    
+            v1 = "-"
         try:
             v2 = string2[i]
         except:
             v2 = "-"
-        
+
         # If the character in both strings is the same, puts that character in
         # the output strings in green, and adds 1 to the Compare variable
-        if max(v1,v2) == min(v1,v2):
-            out1 = out1 + Colors.Green(v1)
-            out2 = out2 + Colors.Green(v2)
+        if max(v1, v2) == min(v1, v2):
+            out1 = out1 + colors.green(v1)
+            out2 = out2 + colors.green(v2)
             compare += 1
-        
+
         # If they are not the same, puts that character in the output strings
         # in red.
         else:
-            out1 = out1 + Colors.Red(v1)
-            out2 = out2 + Colors.Red(v2)
-        
-        # Move to next character 
+            out1 = out1 + colors.red(v1)
+            out2 = out2 + colors.red(v2)
+
+        # Move to next character
         i += 1
-    
+
     # Calculate overlap in %
     overlap = compare/length*100
-    
-    # Resets the coloring after each output string so it won't stain the rest 
+
+    # Resets the coloring after each output string so it won't stain the rest
     # of the output
     out1 = out1 + f'{Style.RESET_ALL}'
     out2 = out2 + f'{Style.RESET_ALL}'
-    
+
     return out1, out2, overlap
+
 
 def is_prime(number):
     prime = True
@@ -424,6 +439,7 @@ def is_prime(number):
             if number % number2:
                 prime = False
     return prime
+
 
 def sentinel():
     run = True
@@ -447,7 +463,7 @@ def is_dna(seq):
 
     '''
     allowed = set('A'+'C'+'T'+'G')
-    if set(seq) <= allowed :
+    if set(seq) <= allowed:
         DNA = True
     else:
         DNA = False
@@ -459,37 +475,38 @@ class colors:
         '''
         Makes input bright green
         Uses Colorama
-        
+
         Parameters
         ----------
         string : Input String
-        
+
         Returns
         -------
         output : Green output string
-        
+
         '''
         output = f'{Fore.GREEN}{Style.BRIGHT}{string}'
         return output
-    
+
     def red(string):
         '''
         Makes input bright red
         Uses Colorama
-        
+
         Parameters
         ----------
         string : Input String
-        
+
         Returns
         -------
         output : Red output string
-        
+
         '''
         output = f'{Fore.RED}{Style.BRIGHT}{string}'
         return output
 
 # Based on class examples
+
 
 def line_counter(file):
     '''
@@ -511,31 +528,32 @@ def line_counter(file):
         # line
         for line in opened_file:
             counter += 1
-            line = line.replace("/n","")
+            line = line.replace("/n", "")
             print(line)
     print(counter)
     return(counter)
+
 
 class tutor_tasks:
     def weekopdracht_2(file):
         '''
         This function takes the input file, and prints the GC% and length
-        
+
         Parameters
         ----------
         file : The FASTA file to be examined
-        
+
         Returns
         -------
         GC100 : The GC% of the gene in the FASTA file, in %
         length: The length of the gene in the FASTA file.
-        
+
         '''
         # Leest de FNA file opgegeven
         string = read_fna(file)
-        #Berekent de waardes
+        # Berekent de waardes
         A, G, C, T, length = agct_count(string)
-        GC=gc_percent(G, C, length)
+        GC = gc_percent(G, C, length)
         # Output de GC% met 2 decimalen achter de komma
         GC100 = "{:.2f}".format(GC*100)
         print(f'GC% = {GC100}%')
@@ -545,12 +563,13 @@ class tutor_tasks:
 
     def weekopdracht_3(file):
         '''
-        This function takes the input file, and prints the D, E, R, K and length
-        
+        This function takes the input file, and prints the D, E, R, K and
+        length
+
         Parameters
         ----------
         file : The FASTA file to be examined
-        
+
         Returns
         -------
         length : The length of the protein in the FASTA file
@@ -565,10 +584,10 @@ class tutor_tasks:
         charge: the charge of the protein
         '''
         string = read_fna(file)
-        d= string.count("D")
-        e= string.count("E")
-        r= string.count("R")
-        k= string.count("K")
+        d = string.count("D")
+        e = string.count("E")
+        r = string.count("R")
+        k = string.count("K")
         length = len(string)
         dp = d / length
         ep = e / length
@@ -579,7 +598,7 @@ class tutor_tasks:
         print(f'Lengte = {length}')
         print(f'Lading = {charge}')
         return length, d, e, r, k, dp, ep, rp, kp, charge
-    
+
     def weektaak_4(file):
         string = read_fna(file)
         weight = protein_weight(string)
@@ -589,17 +608,17 @@ class tutor_tasks:
 class converters:
     def gpio_to_binary(GPIO):
         '''
-        This code converts GPIO input to binary, with the assumption that 101 = 1 
-        and 100 = 0
-    
+        This code converts GPIO input to binary, with the assumption that 101
+        = 1 and 100 = 0
+
         Parameters
         ----------
         GPIO : GPIO input
-    
+
         Returns
         -------
         binary : binary string
-    
+
         '''
         i = 0
         binary = []
@@ -610,31 +629,30 @@ class converters:
                 try:
                     if GPIO[i+2] == '1':
                         binary.append('1')
-                    else: 
+                    else:
                         binary.append('0')
                 except IndexError:
                     break
                 i += 3
         binary = ''.join(binary)
         return binary
-    
-    
+
     def binary_to_text(binary):
         '''
         Converts binary input to string through the ASCII format.
-    
+
         Parameters
         ----------
         binary : Binary input
-    
+
         Returns
         -------
         text : string
-    
+
         '''
-        binint = int(binary, 2);
+        binint = int(binary, 2)
         byte = binint.bit_length() + 7 // 8
-        binarray = binint.to_bytes(byte,'big')
+        binarray = binint.to_bytes(byte, 'big')
         ascii_text = binarray.decode()
         text = ascii_text.strip('\x00')
         return text
