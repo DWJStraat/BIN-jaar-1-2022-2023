24c42e48506a78503e9a6ebb23e814ddbb079ec1
Mobborn02 <dstraat1@gmail.com>
2022-11-11 09:30:50 +0100

    Fixed the consensus_en_profiel.py main

--- David/Afvinkopdrachten/Afvinkopdrachten OWE 2/Afvinkopdracht 1/consensus_en_profiel.py
+++ David/Afvinkopdrachten/Afvinkopdrachten OWE 2/Afvinkopdracht 1/consensus_en_profiel.py
@@ -6,14 +6,47 @@ def most_frequent(list):
     '''
     return max(set(list), key = list.count)
 
+def multipleFna(file_name):
+    """
+    Opens a FNA file, and returns the contents as a list of strings separated
+    at the headers.
+
+    Parameters
+    ----------
+    file_name : Str
+        The path of the file to be opened.
+
+    Returns
+    -------
+    list : List
+        The contents of the file, in a list of strings, separated at the
+        headers.
+    """
+    with open(file_name, "r") as file:
+        content = file.readlines()[1:]
+        full_sequence = ""
+        for line in content:
+            if line[0] != ">":
+                full_sequence += line.strip()
+            else:
+                full_sequence += "\n"
+        list = full_sequence.split("\n")
+    return list
+
+def consensusGenerator(list):
+    for i in range(len(list[0])):
+        column = []
+        for sequence in list:
+            column.append(sequence[i])
+        print(most_frequent(column), end = "")
+
+
 def main(training = False):
     '''
     Determines the consensus sequence of a set of sequences.
     Variables:
         training: boolean, if True, the training file is used, if False, the test file is used.
     '''
-    # Some empty variables to be used later
-    list = []
     # Defining the file names
     training_file = "consensus_en_profiel_training.fa"
     test_file = "consensus_en_profiel_test.fa"
@@ -23,21 +56,9 @@ def main(training = False):
     else:
         filename = test_file
     # Opening the file
-    with open(filename, "r") as file:
-        content = file.readlines()[1:]
-        full_sequence = ""
-        for line in content:
-            if line[0] != ">":
-                full_sequence += line.strip()
-            else:
-                full_sequence += "\n"
-        list = full_sequence.split("\n")
-        print(list)
+    list = multipleFna(filename)
     # Determining the consensus sequence
-    for i in range(len(list[0])):
-        column = []
-        for sequence in list:
-            column.append(sequence[i])
-        print(most_frequent(column), end = "")
+    consensusGenerator(list)
+
 
 main(True)
