7c58308b63762c6188fe742cc0372e71dfb4845d
Mobborn02 <dstraat1@gmail.com>
2022-11-16 11:11:21 +0100

    Updated PrimerDesign.py to use a function. Now actually functioning, too

--- David/PrimerDesign/PrimerDesign.py
+++ David/PrimerDesign/PrimerDesign.py
@@ -9,44 +9,93 @@ import HORUS
 fullDNA = HORUS.read.fna('gene.fna')
 
 CodingDNA = HORUS.read.fna('coding.fna')
-temperature = {'A': 2, 'T': 2, 'G': 4, 'C': 4}
-reverse = {'A':'T', 'T':'A', 'G':'C', 'C':'G'}
-mintemp = 50
-maxtemp = 60
-targetsize = 20
-intronDNA = fullDNA.replace(CodingDNA, '\n')
 
-intronDNA = intronDNA.split('\n')
 
-print(intronDNA)
-intronDNA[0] = intronDNA[0][::-1]
+def primerDesign(fulldna, codingdna, mintemp=50, maxtemp=65, maxtemprange=4,
+                 minsize=18, maxsize=25, startingpos=0):
+    """
+    Design a primer for the DNA provided
 
-primertemp1 = 0
-counter1 = 0
-primer1 = ''
-for i in intronDNA[0]:
-    primertemp1 += temperature[i]
-    primer1 += reverse[i]
-    counter1 += 1
-    if counter1 < 20 and primertemp1 < 60:
-        continue
-    elif counter1 > 20 and 60 > primertemp1 > 50 or primertemp1 > 60:
-        break
-print(f'Primer 1 temperature:{primertemp1}Celcius')
-print(f"Primer 1: 5' {primer1[::-1]} 3'")
-print(f'Primer 1 Length: {len(primer1)}')
+        Parameters
+    ----------
+    fullDNA : str
+        The DNA sequence to be used
+    codingdna : str
+        The coding part of the DNA sequence
+    mintemp : int
+        The minimum temperature of the primer
+    maxtemp : int
+        The maximum temperature of the primer
+    maxtemprange : int
+        The maximum temperature range of the primer
+    minsize : int
+        The minimum size of the primer
+    maxsize : int
+        The maximum size of the primer
+    startingpos : int
+        The starting position of the primer
 
-primertemp2 = 0
-counter2 = 0
-primer2 = ''
-for i in intronDNA[1]:
-    primertemp2 += temperature[i]
-    primer2 += reverse[i]
-    counter2 += 1
-    if counter2 > targetsize and primertemp2 < mintemp:
-        continue
-    if mintemp < primertemp2 < maxtemp:
-        break
-print(f'Primer 2 temperature:{primertemp2} Celcius')
-print(f"Primer 2: 5' {primer2} 3'")
-print(f'Primer 2 Length: {len(primer2)}')
+    Returns
+    -------
+    primerforward: str
+        The forward primer, returned in format 5' [] 3'
+    primerreverse: str
+        The reverse primer, returned in format 5' [] 3'
+    primertempforward: int
+        The temperature of the forward primer
+    primertempreverse: int
+        The temperature of the reverse primer
+    """
+
+    temperature = {'A': 2, 'T': 2, 'G': 4, 'C': 4}
+    reverse = {'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G'}
+    intron_dna = fulldna.replace(codingdna, '\n')
+
+    intron_dna = intron_dna.split('\n')
+
+    intron_dna[0] = intron_dna[0][::-1]
+
+    primertempforward = 0
+    counterforward = 0
+    primerforward = ''
+    primertempreverse = 0
+    counterreverse = 0
+    primerreverse = ''
+    for i in range(startingpos, min(len(intron_dna[0]), len(intron_dna[1]))):
+        if abs(primertempforward - primertempreverse) < maxtemprange:
+            primertempforward += temperature[intron_dna[0][i]]
+            primerforward += reverse[intron_dna[0][i]]
+            primertempreverse += temperature[intron_dna[1][i]]
+            primerreverse += intron_dna[1][i]
+            counterforward += 1
+            counterreverse += 1
+        else:
+            if primertempforward < primertempreverse:
+                primertempforward += temperature[intron_dna[0][i]]
+                primerforward += reverse[intron_dna[0][i]]
+                counterforward += 1
+            else:
+                primertempreverse += temperature[intron_dna[1][i]]
+                primerreverse += intron_dna[1][i]
+                counterreverse += 1
+        if primertempforward + 4 >= maxtemp or \
+                primertempreverse + 4 >= maxtemp:
+            break
+        if counterforward >= maxsize or counterreverse >= maxsize:
+            break
+    primerforward = primerforward[::-1]
+    return primerforward, primerreverse, primertempforward, primertempreverse
+
+
+a1, a2, b1, b2 = primerDesign(fullDNA, CodingDNA)
+print('Forward Primer')
+print('--------------')
+print(f'Temperature:{b1} Celcius')
+print(f"Sequence: 5' {a1} 3'")
+print(f'Length: {len(a1)}')
+print('\n')
+print('Reverse Primer')
+print('--------------')
+print(f'Temperature:{b2} Celcius')
+print(f"Sequence: 5' {a2} 3'")
+print(f'Length: {len(a2)}')
