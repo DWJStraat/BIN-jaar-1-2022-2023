2c90ff83cb7d03ca18f41fda927e75d8fe5a0e0b
Mobborn02 <dstraat1@gmail.com>
2022-12-05 12:18:25 +0100

    Updated Afvinkopdracht 5.py to now have a GUI

--- David/Afvinkopdrachten/Afvinkopdrachten OWE 2/Afvinkopdracht 5/Afvinkopdracht 5.py
+++ David/Afvinkopdrachten/Afvinkopdrachten OWE 2/Afvinkopdracht 5/Afvinkopdracht 5.py
@@ -1,7 +1,38 @@
 import re
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog as fd
+from tkinter.messagebox import showinfo
+import sys
 
 
-class DNA():
+class fasta:
+
+    def __init__(self):
+        self.header = None
+        self.sequence = None
+        self.type = None
+
+    def setHeader(self, header):
+        if header.startswith('>') and not header.isdigit():
+            self.header = header
+        else:
+            print('Invalid header')
+
+    def getHeader(self):
+        return self.header
+
+    def setSequence(self, sequence):
+        if re.search('^[AGCTN]*$', sequence):
+            self.sequence = sequence
+        else:
+            print('Invalid DNA sequence')
+
+    def getSequence(self):
+        return self.sequence
+
+
+class DNA:
     def __init__(self, sequence):
         self.sequence = sequence
 
@@ -32,9 +63,88 @@ class DNA():
         return GCpercent
 
 
-a = DNA('AAA')
+def select_file():
+    """
+    Select a file from the file dialog
 
-a.setDNA('AAAA')
+    Parameters:
+    ----------
+    None
 
-print(a.getDNA())
+    Returns:
+    -------
+    filename: str
+        The name of the file selected
+    """
+    filename = fd.askopenfilename(
+        title='Open a file'
+        # initialdir=r'C:\Users\dstra\OneDrive - HAN\Projectgroep_9\Eno1_FASTAs'
+    )
+    return filename
 
+
+def multiple_fna(file_name):
+    """
+    Opens an FNA file, and returns the contents as a list of strings
+    separated at the headers.
+
+    Parameters
+    ----------
+    file_name : Str
+        The path of the file to be opened.
+
+    Returns
+    -------
+    outlist : List
+        The contents of the file, in a list of strings, separated at the
+        headers.
+    """
+    print("Opening file...")
+    with open(file_name, "r") as file:
+        content = file.readlines()
+        full_sequence = ""
+        for line in content:
+            if line[0] != ">":
+                full_sequence += line.strip()
+            else:
+                full_sequence += f"\n{line.strip()}|"
+        outlist = full_sequence.split("\n")
+    print('File opened!')
+    return outlist[1:]
+
+
+def backend():
+    objectlist = []
+    filename = select_file()
+    for i in multiple_fna(filename):
+        templist = i.split('|')
+        tempobject = fasta()
+        tempobject.setHeader(templist[0])
+        tempobject.setSequence(templist[1])
+        objectlist.append(tempobject)
+    highestgc = 0
+    highestgcobject = None
+    for j in objectlist:
+        tempDna = DNA(j.getSequence())
+        if tempDna.getGcPercent() > highestgc:
+            highestgc = tempDna.getGcPercent()
+            highestgcobject = j
+    showinfo(
+        title='Highest GC content',
+        message=f'The sequence with the highest GC content is \n{highestgcobject.getHeader()}\n with a GC content of {highestgc*100:.2f}%')
+
+
+
+def main():
+    root = tk.Tk()
+    root.title('Object Oriented DNA Sequence Analyzer')
+    root.resizable(True, True)
+    root.geometry('150x150')
+    open_button = ttk.Button(root, text='Open file', command=backend)
+    open_button.pack(expand=True)
+
+    # run the application
+    root.mainloop()
+
+
+main()
