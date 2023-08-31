eb63a167c50f48e86b993bd51ee41f3ad459b1e7
Dstra <64135655+DWJStraat@users.noreply.github.com>
2023-08-31 12:25:33 +0200

    Did some rewriting

--- David/Afvinkopdrachten/Afvinkopdrachten OWE 1/afvinkopdracht 2/Personal Information.py
+++ David/Afvinkopdrachten/Afvinkopdrachten OWE 1/afvinkopdracht 2/Personal Information.py
@@ -1,4 +1,4 @@
-print("I am David Straat. \n"
+print("I am D**** Straat. \n"
       "I live on *******, *****, Utrecht, Netherlands. \n"
       "My phone nr. is 06-*******. \n"
       "My college major is Bio-Informatics")
